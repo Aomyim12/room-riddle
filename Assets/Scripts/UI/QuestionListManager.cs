c151658a611948ff338d75072986ebe01fc85b8e
Aomyim12 <hsamama98@gmail.com>
2025-04-29 22:26:13 +0700

    update QuestionListManager.cs

--- Assets/Scripts/UI/QuestionListManager.cs
+++ Assets/Scripts/UI/QuestionListManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,19 +8,55 @@ public class QuestionListManager : MonoBehaviour
     public Transform contentPanel;
     public GameObject questionItemPrefab;
     public QuestionEditor editor;
+    public TMP_Text filterText;
 
     public List<QuestionData> questionList = new List<QuestionData>();
 
-    void LoadQuestions()
+    private string currentFilter = "";
+
+    private void Start()
     {
+        RefreshList();
+    }
+
+    public void RefreshList()
+    {
+        // Clear existing items
+        foreach (Transform child in contentPanel)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Create filtered items
         foreach (var question in questionList)
         {
-            GameObject item = Instantiate(questionItemPrefab, contentPanel);
-            item.GetComponentInChildren<Text>().text = question.mode;
-            item.transform.Find("EditButton").GetComponent<Button>().onClick.AddListener(() => {
-                editor.LoadQuestion(question);
-            });
+            if (string.IsNullOrEmpty(currentFilter) ||
+                question.questionText.Contains(currentFilter) ||
+                question.mode.Contains(currentFilter))
+            {
+                CreateQuestionItem(question);
+            }
         }
     }
 
+    private void CreateQuestionItem(QuestionData question)
+    {
+        GameObject item = Instantiate(questionItemPrefab, contentPanel);
+        item.GetComponentInChildren<TextMeshProUGUI>().text = $"{question.mode}: {question.questionText}";
+
+        item.transform.Find("EditButton").GetComponent<Button>().onClick.AddListener(() => {
+            editor.LoadQuestion(question);
+        });
+
+        item.transform.Find("DeleteButton").GetComponent<Button>().onClick.AddListener(() => {
+            questionList.Remove(question);
+            RefreshList();
+        });
+    }
+
+    public void SetFilter(string filter)
+    {
+        currentFilter = filter;
+        RefreshList();
+    }
 }
