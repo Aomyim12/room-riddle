71a6e41c0056cf577bfabcfb8bb7f49c68ddf762
Aomyim12 <hsamama98@gmail.com>
2025-04-29 19:35:40 +0700

    script QuestionListManager.cs

---
+++ Assets/Scripts/QuestionListManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestionListManager : MonoBehaviour
+{
+    public Transform contentPanel;
+    public GameObject questionItemPrefab;
+    public QuestionEditor editor;
+
+    void LoadQuestions()
+    {
+        foreach (var question in questionList)
+        {
+            GameObject item = Instantiate(questionItemPrefab, contentPanel);
+            item.GetComponentInChildren<Text>().text = question.title;
+            item.transform.Find("EditButton").GetComponent<Button>().onClick.AddListener(() => {
+                editor.LoadQuestion(question);
+            });
+        }
+    }
+
+}
