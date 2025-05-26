af0bb38d22e6580229133ae300a37436b65c5e1a
Aomyim12 <hsamama98@gmail.com>
2025-05-26 14:49:02 +0700

    Choose Room

---
+++ Assets/Scripts/RandomSceneLoader.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class RandomSceneLoader : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    public string[] sceneNames = { "1stRoom", "2ndRoom", "3ndRoom" };
+    public float delayBeforeLoad = 0.5f;
+
+    private Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClicked);
+    }
+
+    private void OnButtonClicked()
+    {
+        // ปิดการคลิกชั่วคราว
+        button.interactable = false;     
+
+        // สุ่มเลือก Scene
+        string sceneToLoad = sceneNames[Random.Range(0, sceneNames.Length)];
+        Debug.Log("กำลังโหลด: " + sceneToLoad);
+
+        // โหลด Scene หลังดีเลย์
+        Invoke(nameof(LoadRandomScene), delayBeforeLoad);
+    }
+
+    private void LoadRandomScene()
+    {
+        string sceneToLoad = sceneNames[Random.Range(0, sceneNames.Length)];
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
