8c9ad78079c92f0b3d21265d77165868dc978cc4
Aomyim12 <hsamama98@gmail.com>
2025-11-12 01:17:43 +0700

    Menu UI

--- Assets/Scripts/UI/MultiSceneButton.cs
+++ Assets/Scripts/UI/MultiSceneButton.cs
@@ -13,7 +13,7 @@ public class SceneChanger : MonoBehaviour
     // Call this function from your button OnClick() event
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene("ChooseRoom");
     }
     // Quit the game
     public void QuitGame()
